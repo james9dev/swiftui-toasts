0e1d28b2a0707b0352855afe79c9569f3bda9bc7
James <james@Jamess-MacBook-Pro.local>
2025-05-22 14:22:04 +0900

    Update to v1.0.3

--- Package.swift
+++ Package.swift
@@ -11,7 +11,7 @@ let package = Package(
       targets: ["Toasts"])
   ],
   dependencies: [
-    .package(url: "https://github.com/sunghyun-k/swiftui-window-overlay.git", from: "1.0.2")
+    .package(url: "https://github.com/sunghyun-k/swiftui-window-overlay.git", from: "1.0.3")
   ],
   targets: [
     .target(
